6b86ff8cbe071f15a5923e3efd8c6e265da0f62d
Gabriele-Viola <gabrieleviola50@gmail.com>
2024-10-11 14:35:53 +0200

    add first snack

--- Snack_1/main.js
+++ Snack_1/main.js
@@ -1,3 +1,64 @@
 /* Creare un array di oggetti:
  Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
  Stampare a schermo la bici con peso minore.*/
+const h_1El = document.getElementById('printWeight')
+
+const Bycycles =
+    [
+
+        {
+            Name: 'Bike1',
+            weight: 5,
+        },
+        {
+            Name: 'Bike2',
+            weight: 8,
+        },
+        {
+            Name: 'Bike3',
+            weight: 9,
+        },
+        {
+            Name: 'Bike4',
+            weight: 10,
+        },
+        {
+            Name: 'Bike5',
+            weight: 2,
+        },
+        {
+            Name: 'Bike6',
+            weight: 3,
+        },
+        {
+            Name: 'Bike7',
+            weight: 4,
+        },
+        {
+            Name: 'Bike8',
+            weight: 4,
+        },
+        {
+            Name: 'Bike9',
+            weight: 3,
+        }, {
+            Name: 'Bike10',
+            weight: 6,
+        },
+    ]
+
+    let minWeight = [100]
+    for (let i = 0; i < Bycycles.length-1; i++) {
+        const bike = Bycycles[i].weight;
+        if (Bycycles[i].weight < Bycycles[i+1].weight && Bycycles[i].weight < minWeight[0]) {
+            minWeight.pop()
+            minWeight.push(Bycycles[i].weight)
+            
+        }else if (Bycycles[i].weight > Bycycles[i+1].weight && Bycycles[i].weight < minWeight[0]) {
+            minWeight.pop()
+            minWeight.push(Bycycles[i+1].weight)
+        }
+        
+    }
+   console.log(minWeight[0]);
+   h_1El.innerText = minWeight[0]
