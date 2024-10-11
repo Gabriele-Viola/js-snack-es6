d1a68dd8a60604df55544ff4a183216864c35f16
Gabriele-Viola <gabrieleviola50@gmail.com>
2024-10-11 14:08:53 +0200

    initial commit

---
+++ Snack_3_Bonus/main.js
@@ -0,0 +1,3 @@
+/* Creare un array di oggetti di squadre di calcio.
+Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
+La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.*/
